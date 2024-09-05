bfcc17b1f72845ff0c046f3494242fb1be7fb9d3
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-09-05 17:30:31 -0400

    fix: possibly fix to date issues

--- discord-bot/sdk/date.ts
+++ discord-bot/sdk/date.ts
@@ -1,13 +1,22 @@
-export const dateInSeconds = (date: string | Date) =>
-  Math.floor(
+export function dateInSeconds(date: string | Date) {
+  return Math.floor(
     new Date(date).getTime() / 1000,
   );
+}
 
-export const isToday = (date: string | Date) => {
-  const today = new Date();
-  const createdAt = new Date(date);
+function getDateFromSaoPauloTZ(date: string | Date) {
+  const [day, month, year] = new Date(date)
+    .toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" })
+    .split(", ")?.[0]?.split("/");
 
-  return createdAt.getDate() === today.getDate() &&
-    today.getMonth() === createdAt.getMonth() &&
-    today.getFullYear() === createdAt.getFullYear();
-};
+  return { year, month, day };
+}
+
+export function isToday(date: string | Date) {
+  const today = getDateFromSaoPauloTZ(new Date());
+  const _date = getDateFromSaoPauloTZ(new Date(date));
+
+  return _date.day === today.day &&
+    today.month === _date.month &&
+    today.year === _date.year;
+}
